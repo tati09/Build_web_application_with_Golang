623260424bf070a5f4a12bcb5e8fde5c43ae0675
Tachibana <siy.tachibanana@gmail.com>
2019-05-19 21:38:27 +0900

    test_go

---
+++ src/mymath/sqrt.go
@@ -0,0 +1,10 @@
+// $GOPATH/src/mymath/sqrt.goコードは以下の通り：
+package mymath
+
+func Sqrt(x float64) float64 {
+    z := 0.0
+    for i := 0; i < 1000; i++ {
+        z -= (z*z - x) / (2 * x)
+    }
+    return z
+}
